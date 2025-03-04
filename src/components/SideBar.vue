a463b4b38ad62bab2aafbe0997a559e41f3f8a0e
Brnt_Luap <paul.bournet@orange.fr>
2025-03-04 00:55:05 -0500

    Début complétion de la sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,16 @@
 <template>
-    <aside class=" fixed overflow-hidden top-0 right-0 h-full rounded-lg">
-        <div class="top-0 right-0 h-full relative bg-gray-900 text-white">
-            <h1 class="flex justify-between items-center p-3 bg-gray-500 border-b-1 border-white  font-size-26">
+    <aside class="fixed overflow-hidden top-20 right-0 h-full rounded-lg">
+        <div class="relative h-full bg-gray-100 text-white">
+            <h1 class="flex justify-between items-center p-3 bg-gray-500 border-b border-white text-lg italic text-black">
+                Ajouter une destination
+                <button class="ml-5 text-lg font-bold text-red-600" @click="toggle">&times;</button>
             </h1>
         </div>
     </aside>
 </template>
+
+<script>
+export default {
+  props: ['toggle']
+}
+</script>
