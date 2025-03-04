10f448bed416fea1aceef8b7dd05ec6b1723056e
Brnt_Luap <paul.bournet@orange.fr>
2025-03-04 13:30:08 -0500

    Ajout des boutons pour le choix du statut

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -5,6 +5,29 @@
                 Ajouter une destination
                 <button class="ml-5 text-lg font-bold text-red-600" @click="toggle">&times;</button>
             </h1>
+                <!-- Conteneur des boutons -->
+                <form >
+                    <ul class="flex space-x-1 mt-4">
+                        <li>
+                            <input type="radio" id="toggle1" class="peer hidden" name="choice" value="toggle1"/>
+                            <label for="toggle1" class="focus:outline-none text-white bg-green-200 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 border border-black peer-checked:bg-green-700">
+                                To Do
+                            </label>
+                        </li>
+                        <li>
+                            <input type="radio" id="toggle2" class="peer hidden" name="choice" value="toggle2"/>
+                            <label for="toggle2" class="focus:outline-none text-white bg-green-200 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 border border-black peer-checked:bg-green-700">
+                                Doing
+                            </label>
+                        </li>
+                        <li>
+                            <input type="radio" id="toggle3" class="peer hidden" name="choice" value="toggle3"/>
+                            <label for="toggle3" class="focus:outline-none text-white bg-green-200 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 border border-black peer-checked:bg-green-700">
+                                Done
+                            </label>
+                        </li>
+                    </ul>
+                </form>
         </div>
     </aside>
 </template>
