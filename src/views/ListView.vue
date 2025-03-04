bcd90fabf2c931e3f8e1907b61f69600f14f4980
Brnt_Luap <paul.bournet@orange.fr>
2025-03-03 23:05:52 -0500

    Button add v2

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -4,7 +4,7 @@
     <nav class="border-gray-200">
         <div class="space-y-4 mx-auto flex flex-col items-center justify-center text-center">
             <div>
-                <button>Test</button>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
             </div>
             <div id="todo">
                 <span class="text-4xl font-bold text-black block">To do</span>
