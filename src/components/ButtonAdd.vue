11f92d27fba0859920ffc366a0a44fee8e709dc1
Brnt_Luap <paul.bournet@orange.fr>
2025-03-04 00:12:41 -0500

    Create button add component

---
+++ src/components/ButtonAdd.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1/2 px-1 rounded inline-flex items-center">
+        <span>+</span>
+    </button>
+</template>
