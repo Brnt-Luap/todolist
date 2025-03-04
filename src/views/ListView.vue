e7c966e25af0113ee06e6ace5c78c88207b0d04f
Brnt_Luap <paul.bournet@orange.fr>
2025-03-04 00:09:50 -0500

    Taille bouton add

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -6,7 +6,7 @@
             <div class="flex flex-col items-center">
                 <div id="todo" class="flex items-center space-x-4">
                     <span class="text-4xl font-bold text-black leading-tight">To do</span>
-                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 rounded inline-flex items-center">
+                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1/2 px-1 rounded inline-flex items-center">
                         <span>+</span>
                     </button>
                 </div>
