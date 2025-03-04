70112a94c29712d2ccd62746ed7fb00fd97884b5
Brnt_Luap <paul.bournet@orange.fr>
2025-03-04 14:32:35 -0500

    Border of sidebard

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <aside class="fixed overflow-hidden top-20 right-1 h-60 rounded-lg">
+    <aside class="fixed overflow-hidden top-20 right-1 h-60 rounded-lg border border-black">
         <div class="relative h-full bg-gray-100 text-white">
             <h1 class="flex justify-between text-justify items-center p-3 bg-gray-500 border-b border-white text-lg italic text-black">
                 Add a destination
