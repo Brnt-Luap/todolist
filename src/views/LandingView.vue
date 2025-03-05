8826794c3d56ef8a7beb63fa789495852943fd4d
Ines-El-Ghouaouta <i.elghouaouta@gmail.com>
2025-03-04 22:29:53 -0500

    page profil + image list

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -5,8 +5,13 @@
         <div class="space-y-4">
             <!-- Texte principal -->
             <div>
-                <span class="text-4xl font-bold text-black block">Travel To Do List</span>
-                <p class="mb-4 font-light mt-1 italic">“ Life is either a daring adventure or nothing at all. “ - Helen Keller</p>
+                <span class="text-4xl font-bold text-black block flex justify-center mt-5">Travel To Do List</span>
+                 <blockquote class="text-xl italic font-semibold text-gray-900 dark:text-black mb-10 mt-10">
+                  <svg class="w-8 h-8 text-gray-400 dark:text-gray-600 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                  <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+                  </svg>
+                  <p>“ Life is either a daring adventure or nothing at all. “ - Helen Keller</p>
+                </blockquote>
                 <img src="../assets/landing/landing.png" alt="ToDo" class="rounded-md pb-5">
             </div>
 
@@ -28,7 +33,6 @@
                     <p class="text-gray-700 font-medium font-bold leading-tight">Done</p>
                     <p class="text-gray-700 font-small italic leading-tight">From dream to reality...</p>
                 </div>
-                
             </div>
         </div>
     </nav>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,10 +1,78 @@
 <template>
-    <HeroHeader/>
+<HeroHeader/>
+<div class="bg-montreal p-8 rounded-md shadow-md">
+  <div class="flex items-center space-x-4 p-4 bg-white/80 rounded-md shadow-lg backdrop-blur-sm">
+    <img src="../assets/profil/homme.jpg" alt="User" class="rounded-md w-52 h-52">
     <div>
-        <p>Profile View</p>
+      <h2 class="text-lg font-semibold">Omar Benkacem</h2>
+      <p class="text-gray-600">omar.benkacem@email.com</p>
+      <p class="text-gray-700">🌍 15 villes visitées</p>
+      <p class="text-gray-500">📅 Inscrit depuis 2022</p>
+      <p><router-link to="/login"><button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Log out</button></router-link></p>
     </div>
+  </div>
+</div>
+<div class="flex items-center space-x-4 p-4 bg-gray-100 rounded-md shadow-md">
+  <img src="../assets/profil/beijing1.avif" alt="B1" class="rounded-md w-[130px] h-[130px]">
+  <img src="../assets/profil/beijing2.jpeg" alt="B2" class="rounded-md w-[130px] h-[130px]">
+  <img src="../assets/profil/beijing3.avif" alt="B3" class="rounded-md w-[130px] h-[130px]">
+  <div>
+    <h2 class="text-lg font-semibold">Beijing - China</h2>
+    <p class="text-gray-700">Date: April 19-26 2023</p>
+    <p class="text-gray-700">Place visited: The Forbidden City - The Temple of Heaven - The Great Wall of China</p>
+    <div class="flex items-center">
+        <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        <svg class="w-4 h-4 text-gray-300 me-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">4.95</p>
+        <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">out of</p>
+        <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">5</p>
+    </div>
+  </div>
+</div>
+<div class="flex items-center space-x-4 p-4 bg-gray-100 rounded-md shadow-md">
+  <img src="../assets/profil/mar1.webp" alt="M1" class="rounded-md w-[130px] h-[130px]">
+  <img src="../assets/profil/mar2.jpg" alt="M2" class="rounded-md w-[130px] h-[130px]">
+  <img src="../assets/profil/mar3.webp" alt="M3" class="rounded-md w-[130px] h-[130px]">
+  <div>
+    <h2 class="text-lg font-semibold">Marrakech - Morocco</h2>
+    <p class="text-gray-700">Date: July 10-17 2024</p>
+    <p class="text-gray-700">Place visited: Jemaa el-Fnaa - Saadian Tombs - Ben Youssef Madrasa</p>
+    <div class="flex items-center">
+        <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        <svg class="w-4 h-4 text-gray-300 me-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">4.95</p>
+        <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">out of</p>
+        <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">5</p>
+    </div>
+  </div>
+</div>
 </template>
-
 <script>
 import HeroHeader from '@/components/HeroHeader.vue'
 export default {
