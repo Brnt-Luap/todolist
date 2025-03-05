5633e6666311042fa3acf15db66bd2691b4a44d8
Brnt_Luap <paul.bournet@orange.fr>
2025-03-05 09:18:52 -0500

    Modification accès login page

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="mx-15 max-w-screen-xl px-20 lg:px-50 py-1 border-b">
             <div class="bg-red flex flex-wrap justify-between items-center">
-                <router-link to="/login"><h1 class="text-4xl tracking-tight"> TripGoal </h1>
+                <router-link to="/"><h1 class="text-4xl tracking-tight"> TripGoal </h1>
 </router-link>
             </div>
     </header>
