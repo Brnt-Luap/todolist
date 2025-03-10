099b7be40809dbef81e953c072e15df1fec013d7
Brnt_Luap <paul.bournet@orange.fr>
2025-03-10 14:06:39 -0400

    Profile Page Update

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -14,17 +14,13 @@
   </div>
 </div>
 <div class="flex items-center space-x-4 p-4 bg-gray-100 rounded-md shadow-md">
-  <img src="../assets/profil/china.png" alt="B1" class="rounded-md w-[130px] h-[130px]">
-  <!--
-  <img src="../assets/profil/beijing2.jpeg" alt="B2" class="rounded-md w-[130px] h-[130px]">
-  <img src="../assets/profil/beijing3.avif" alt="B3" class="rounded-md w-[130px] h-[130px]">
-  -->
+  <img src="../assets/flags/china.png" alt="B1" class="rounded-md w-[130px] h-[130px]">
   <div>
     <h2 class="text-lg font-semibold">Beijing - China</h2>
-    <p class="text-gray-700">Date: {{dateStartDone}}</p>
-    <p class="text-gray-700">Place visited: The Forbidden City - The Temple of Heaven - The Great Wall of China</p>
+    <p class="text-gray-700">Date: {{getdate}}</p>
+    <!-- <p class="text-gray-700">Place visited: The Forbidden City - The Temple of Heaven - The Great Wall of China</p> -->
+    <!-- Rate
     <div class="flex items-center">
-      <!-- Rate -->
         <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
         </svg>
@@ -44,15 +40,15 @@
         <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">out of</p>
         <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">5</p>
     </div>
+    -->
   </div>
 </div>
 <div class="flex items-center space-x-4 p-4 bg-gray-100 rounded-md shadow-md">
   <img src="../assets/profil/mar1.webp" alt="M1" class="rounded-md w-[130px] h-[130px]">
-  <img src="../assets/profil/mar2.jpg" alt="M2" class="rounded-md w-[130px] h-[130px]">
-  <img src="../assets/profil/mar3.webp" alt="M3" class="rounded-md w-[130px] h-[130px]">
   <div>
     <h2 class="text-lg font-semibold">Marrakech - Morocco</h2>
     <p class="text-gray-700">Date: July 10-17 2024</p>
+    <!--
     <p class="text-gray-700">Place visited: Jemaa el-Fnaa - Saadian Tombs - Ben Youssef Madrasa</p>
     <div class="flex items-center">
         <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
@@ -74,6 +70,7 @@
         <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">out of</p>
         <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">5</p>
     </div>
+    -->
   </div>
 </div>
 </template>
@@ -95,11 +92,8 @@ export default {
     doneCount () {
       return this.task.length > 0 ? this.task[0].done.length : 0
     },
-    getdateStart () {
-      if (this.task.length > 0) {
-        return this.task[0].done.map(item => item.dateStart)
-      }
-      return []
+    getdate () {
+      return this.task?.[0]?.done?.[0]?.date || ''
     }
   }
 }
