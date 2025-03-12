7b2cb0cf403065910b8332d322847752c40eaf97
Brnt_Luap <paul.bournet@orange.fr>
2025-03-12 16:44:45 -0400

    Server Update #1

---
+++ server/app/models/destinations.model.js
@@ -0,0 +1,32 @@
+module.exports = (connex, Sequelize) => {
+    const Destination = connex.define('destination', {
+        id: {
+            type: Sequelize.REAL
+        },
+        city: {
+            type: Sequelize.STRING
+        },
+        description: {
+            type: Sequelize.STRING
+        },
+        description: {
+            type: Sequelize.TEXT
+        },
+        dateStart: {
+            type: Sequelize.STRING
+        },
+        dateEnd: {
+            type: Sequelize.STRING
+        },
+        status: {
+            type: Sequelize.STRING
+        },
+        rate: {
+            type: Sequelize.REAL
+        },
+        place: {
+            type: Sequelize.STRING
+        }
+    })
+    return Destination
+}
