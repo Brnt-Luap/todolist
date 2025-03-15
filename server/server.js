692d23a101ef166570229347658525d5f896f16f
Brnt_Luap <paul.bournet@orange.fr>
2025-03-15 03:05:49 -0400

    server adapt

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+
 const app = express()
 const corsOption = {
     origin: 'http://localhost:8081',
@@ -12,19 +13,14 @@ app.use(cookieParser())
 app.use(cors(corsOption))
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-
-//test
-// app.get('/', (req, res) => {
-//     res.json({message: 'Welcome'})
-// })
-
-const db = require('./app/models')
-db.connex.sync()
-
-require('./app/routes/user.route')(app)
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const PORT = 8080
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
 })
+
+const db = require('./app/models')
+db.connex.sync()
+require('./app/routes/user.route')(app)
+require('./app/routes/destination.route')(app)
