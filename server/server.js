91863fba1b7f963c3164e05514cc995ab54c8e04
Brnt_Luap <paul.bournet@orange.fr>
2025-03-15 02:56:54 -0400

    MàJ

--- server/server.js
+++ server/server.js
@@ -14,11 +14,6 @@ app.use(cors(corsOption))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-//test
-// app.get('/', (req, res) => {
-//     res.json({message: 'Welcome'})
-// })
-
 const db = require('./app/models')
 db.connex.sync()
 
